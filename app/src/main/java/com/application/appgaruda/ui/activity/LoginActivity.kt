41154e98135526876fea79d3a097ab341b9e66fe
Akbar <akbardhia19@gmail.com>
2025-07-24 15:28:22 +0700

    Add : Prototype Login Page

--- app/src/main/java/com/application/appgaruda/ui/activity/LoginActivity.kt
+++ app/src/main/java/com/application/appgaruda/ui/activity/LoginActivity.kt
@@ -1,34 +1,23 @@
 package com.application.appgaruda.ui.activity
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.application.appgaruda.R
-import com.application.appgaruda.helper.SessionManager
 
-class SplashActivity : AppCompatActivity() {
-
-
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val Session = SessionManager(this)
 
-        if (Session.isLoggedIn()) {
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
-        } else {
-            startActivity(Intent(this, LoginActivity::class.java))
-            finish()
-        }
+
     }
 }
